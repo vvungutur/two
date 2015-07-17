d3fb28240974baba8527e087fae66fac997babc4
Vamsi <vs.vungutur@gmail.com>
2015-07-16 22:31:17 -0400

    commiting go one

---
+++ main.go
@@ -0,0 +1,50 @@
+package main 
+
+
+import (
+
+	"fmt"
+	"time"	
+
+)
+
+
+func sender(c chan int){
+
+
+	for i:= 0; i <= 3; i ++ {
+
+		c <- i
+		time.Sleep(time.Second * 1)
+
+
+	}
+
+
+}
+
+func receiver(c chan int){
+
+
+	for  {
+
+		msg :=  <- c
+		fmt.Println(msg)
+
+
+	}
+
+
+}
+func main(){
+
+	c := make(chan int)
+
+	go sender(c)
+	go receiver(c)
+
+	var input string
+
+	fmt.Scanln(&input)
+
+}
