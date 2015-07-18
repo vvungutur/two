682c5cdce120a22307cdb425c268f62cc5c9866a
Vamsi <vs.vungutur@gmail.com>
2015-07-18 14:39:07 -0400

    made changes to two - main, removed funcs1

--- goex/hw/src/two/main.go
+++ goex/hw/src/two/main.go
@@ -4,13 +4,49 @@ package main
 import (
 
 	"fmt"
+	"time"
 
 )
 
 
+
+func emit(wordChannel chan string, done chan bool) {
+
+
+	defer close (wordChannel)
+	words := []string{"The", "quick", "brown", "fox"}
+
+	i := 0
+
+	t := time.NewTimer(3 * time.Second)
+
+	for {
+		select {
+		case wordChannel <- words[i]:
+			i += 1
+			if i == len(words){
+				i = 0
+			}
+		case <- done:
+			done <- true
+			return
+		
+		case <- t.C:
+			return
+	
+		}	
+	}
+}
 func main() {
 
-	
-	fmt.Println("hello")
-	alt()
+	wordCh := make(chan string)
+	doneCh := make(chan bool)
+
+	go emit(wordCh, doneCh)
+
+	for word := range wordCh{
+		fmt.Printf("%s", word)
+
+	}	
+
 }
